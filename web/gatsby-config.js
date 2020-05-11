07695f885fcfc0181b2f14d7cdcd51591dbe6a00
Nation <eric.nation@gmail.com>
2020-05-10 19:13:56 -0500

    Add GA to the config

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -23,5 +23,20 @@ module.exports = {
         overlayDrafts: !isProd && token,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'UA-166068421-1',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**', '/do-not-track/me/too/'],
+      },
+    },
   ],
 };
