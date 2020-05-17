cb2cdc1073012764f9c0c6276bc22dfa3fe0c83d
Nation <eric.nation@gmail.com>
2020-05-17 16:02:11 -0500

    Add styles to code block

--- web/src/components/code/index.js
+++ web/src/components/code/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
+import styles from './code.module.css';
 
 const Code = ({ node }) => {
   if (!node || !node.code) {
     return null;
   }
   const { language, code } = node;
-  return <SyntaxHighlighter language={language || 'text'}>{code}</SyntaxHighlighter>;
+  return (
+    <div className={styles.code}>
+      <SyntaxHighlighter language={language || 'text'}>{code}</SyntaxHighlighter>
+    </div>
+  );
 };
 
 export default Code;
